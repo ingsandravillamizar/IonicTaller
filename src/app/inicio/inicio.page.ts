571caa64d618fbb4fee6b560f5e256fed9fd23d2
svillamizar <ingsandravillamizar@github.com>
2023-02-08 11:15:54 -0500

    tercera version

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';  //importanmos
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -8,15 +9,38 @@ import { Router } from '@angular/router';  //importanmos
 })
 export class InicioPage implements OnInit {
   //priveta router: Router para hacer los redireccionamientos
-  constructor(private router:Router) { 
+  constructor(private router:Router, private alertController: AlertController ) { 
 
   }
 
   ngOnInit() {
   }
 
-  subMenu(){
-    this.router.navigate(['/tabs'],{replaceUrl:true});
+  subMenu(path:string){
+
+    this.router.navigate([path],{replaceUrl:true});
   }
 
+
+ async presentAlert() {
+  const alert = await this.alertController.create({
+    header: 'Alert',
+    subHeader: 'Subtitle',
+    message: 'This is an alert message.',
+    buttons: [{
+          text: 'Ruta 3',
+          handler :()=>{
+            this.subMenu('/tabs/ruta-tres');
+          }
+    },
+     
+    {
+      text: 'Ruta 4',
+      handler: ()=>{this.subMenu('/tabs/ruta-cuatro');}
+      
+}]
+  });
+
+  await alert.present();
+}
 }
